c48f1101ff53d3fec8a11231f8a46167e7b3108d
minahermina <mina.albert.saeed@gmail.com>
2025-04-24 05:36:55 +0200

    Edit main.c

--- test/main.c
+++ test/main.c
@@ -1,13 +1,11 @@
-#include<stdio.h>
-#include<arena_allocator.h>
-#include<bignum.h>
+#include <stdio.h>
+#include <bignum.h>
 
 int
 main(){
     Arena arena = {0};
     arena_init(&arena, 2048);
-    BigNum *num = NULL;
-    num = BigNum_new(&arena);
+    BigNum *num = bignum_new(&arena);
     printf("Hello");
     return 0;
 }

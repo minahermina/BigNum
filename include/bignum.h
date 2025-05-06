2560c113dd3499978ea0d53acd90906287c9eb6f
minahermina <mina.albert.saeed@gmail.com>
2025-05-06 07:15:58 +0300

    Fix parameters names in function prototypes in bignum.h

--- include/bignum.h
+++ include/bignum.h
@@ -77,18 +77,18 @@ BigNum *bignum_from_dec(const char *str, size_t len, Arena *arena);
 BigNum *bignum_from_hex(const char *str, size_t len, Arena *arena);
 
 /* Arithmetic */
-int bignum_uadd(BigNum *res, const BigNum *a, const BigNum *b, Arena *arena);
-int bignum_add(BigNum *res, const BigNum *a, const BigNum *b, Arena *arena);
-int bignum_usubtract(BigNum *res, const BigNum *a, const BigNum *b, Arena *arena);
-int bignum_subtract(BigNum *res, const BigNum *a, const BigNum *b, Arena *arena);
-int bignum_mul(BigNum *res, const BigNum *a, const BigNum *b, Arena *arena);
-int bignum_div(BigNum *dv, BigNum *rem, const BigNum *a, const BigNum *d, Arena *arena);
-int bignum_mod(BigNum *rem, const BigNum *a, const BigNum *m, Arena *arena);
-int bignum_gcd(BigNum *res, const BigNum *a, Arena *arena);
+int bignum_uadd(BigNum *res, const BigNum *num1, const BigNum *num2, Arena *arena);
+int bignum_add(BigNum *res, const BigNum *num1, const BigNum *num2, Arena *arena);
+int bignum_usubtract(BigNum *res, const BigNum *num1, const BigNum *num2, Arena *arena);
+int bignum_subtract(BigNum *res, const BigNum *num1, const BigNum *num2, Arena *arena);
+int bignum_mul(BigNum *res, const BigNum *num1, const BigNum *num2, Arena *arena);
+int bignum_div(BigNum *div, BigNum *remain, const BigNum *num1, const BigNum *num2, Arena *arena);
+int bignum_mod(BigNum *remain, const BigNum *num1, const BigNum *num2, Arena *arena);
+int bignum_gcd(BigNum *res, const BigNum *num1, const BigNum *num2, Arena *arena);
 
 /*Bitwise Operation*/
 BigNumWord bignum_lshift_word(BigNumWord word, size_t nbits);
-int bignum_lshift(BigNum *res, BigNum *num, size_t nbits, Arena *num1rena);
+int bignum_lshift(BigNum *res, BigNum *num, size_t nbits, Arena *arena);
 BigNumWord bignum_rshift_word(BigNumWord word, size_t nbits);
 int bignum_rshift(BigNum *res, BigNum *num1, int nbits);
 int bignum_and(BigNum *res, const BigNum *num1, const BigNum *num2, Arena *arena);
@@ -97,8 +97,8 @@ int bignum_xor(BigNum *res, const BigNum *num1, const BigNum *num2, Arena *arena
 
 
 /* Modular Arithmetic */
-int bignum_mod_exp(BigNum *res, const BigNum *a, const BigNum *p, const BigNum *m, Arena *arena);
-int bignum_mod_inverse(BigNum *res, const BigNum *a, const BigNum *n, Arena *arena);
+int bignum_mod_exp(BigNum *res, const BigNum *num1, const BigNum *exp, const BigNum *m, Arena *arena);
+int bignum_mod_inverse(BigNum *res, const BigNum *num1, const BigNum *n, Arena *arena);
 
 int bignum_compare(const BigNum *num1, const BigNum *num2);             /*done*/
 int bignum_ucompare(const BigNum *num1, const BigNum *num2);             /*done*/
